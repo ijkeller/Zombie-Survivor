8b5dd82449b80014a9ca5915072d4dc8e01515b6
Isaiah Keller <isaiah.keller@gmail.com>
2022-06-29 18:03:51 -0700

    working through bug where the sprite is turning every time the action button is pressed

--- player-movement.js
+++ player-movement.js
@@ -2,7 +2,7 @@
 
 let img = new Image();
 img.src = './assets/generated1.png';
-img.onload = function() {
+img.onload = function () {
     window.requestAnimationFrame(gameLoop)
 }
 
@@ -13,7 +13,7 @@ let walkScale = 1;
 let walkWidth = 64;
 let walkHeight = 64;
 
-let swingScale = 3;
+let swingScale = 1;
 let swingWidth = 192;
 let swingHeight = 192;
 
@@ -21,16 +21,16 @@ let scale = walkScale;
 let width = walkWidth;
 let height = walkHeight;
 
-let scaledWidth = scale * width;
-let scaledHeight = scale * height;
+// let scaledWidth = scale * width;
+// let scaledHeight = scale * height;
 
 let moveSpeed = 1;
-let xPosition = 0;
-let yPosition = 0;
+let xPosition = 32;
+let yPosition = 32;
 
 let currentLoopIndex = 0;
 let frameCount = 0;
-let frameLimit = 20;
+let frameLimit = 10;
 
 let walkUp = 8;
 let walkLeft = 9;
@@ -38,20 +38,29 @@ let walkDown = 10;
 let walkRight = 11;
 let walkCycle = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-let swingUp = 7;
+let swingUp = 8;
 let swingLeft = 8;
 let swingDown = 9;
 let swingRight = 10;
 let swingCycle = [0, 1, 2, 3, 4, 5];
+let swing = false;
 
+let currentCycle = walkCycle;
 let currentDirection = walkRight;
 
-function drawFrame( frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(img,
-        frameX * width, frameY * height, width, height,
-        canvasX, canvasY, scaledWidth, scaledHeight);
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+    // console.log(`x = ${xPosition}, y = ${yPosition}`)
+    if (swing) {
+        ctx.drawImage(img,
+            frameX * width, frameY * height, width, height,
+            canvasX - 96, canvasY - 96, scale * width, scale * height);
+        // console.log('drawImage');
+    } else {
+        ctx.drawImage(img,
+            frameX * width, frameY * height, width, height,
+            canvasX - 32, canvasY - 32, scale * width, scale * height);
+    }
 }
-
 
 let keyPresses = {};
 
@@ -61,7 +70,7 @@ function keyDownListener(event) {
     keyPresses[event.key] = true;
 }
 
-window.addEventListener('keyup', keyUpListener, false);
+window.addEventListener('keyup', keyUpListener, true);
 function keyUpListener(event) {
     // event.preventDefault;
     keyPresses[event.key] = false;
@@ -71,38 +80,42 @@ function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     let hasMoved = false;
-    let swing = false;
-    let currentCycle = walkCycle;
 
     if (keyPresses.f) {
+        hasMoved = false;
+        scale = swingScale;
+        width = swingWidth;
+        height = swingHeight;
+        currentCycle = swingCycle;
+        currentLoopIndex = 0;
+        swing = true;
         if (currentDirection == walkUp) {
             currentDirection = swingUp;
-            scale = swingScale;
-            width = swingWidth;
-            height = swingHeight;
-            currentCycle = swingCycle;
-            swing = true;
+            console.log(currentDirection)
         } else if (currentDirection == walkLeft) {
             currentDirection = swingLeft;
-            scale = swingScale;
-            width = swingWidth;
-            height = swingHeight;
-            currentCycle = swingCycle;
-            swing = true;
         } else if (currentDirection == walkDown) {
             currentDirection = swingDown;
-            scale = swingScale;
-            width = swingWidth;
-            height = swingHeight;
-            currentCycle = swingCycle;
-            swing = true;
         } else {
             currentDirection = swingRight;
-            scale = swingScale;
-            width = swingWidth;
-            height = swingHeight;
-            currentCycle = swingCycle;
-            swing = true;
+        }
+    }
+
+    if (swing) {
+        frameCount++;
+        hasMoved = false;
+        // console.log(currentDirection)
+        // console.log(frameCount)
+        // console.log(frameLimit)
+        if (frameCount >= frameLimit) {
+            frameCount = 0;
+            currentLoopIndex++;
+            if (currentLoopIndex >= swingCycle.length) {
+                currentLoopIndex = 0;
+                swing = false;
+                width = walkWidth;
+                height = walkHeight;
+            }
         }
     }
 
@@ -117,27 +130,26 @@ function gameLoop() {
     } else if (keyPresses.s) {
         yPosition += moveSpeed;
         currentDirection = walkDown;
+        scale = walkScale;
+        width = walkWidth;
+        height = walkHeight;
         hasMoved = true;
     }
     if (keyPresses.d) {
         xPosition += moveSpeed;
         currentDirection = walkRight;
+        scale = walkScale;
+        width = walkWidth;
+        height = walkHeight;
         hasMoved = true;
     } else if (keyPresses.a) {
-        xPosition  -= moveSpeed;
+        xPosition -= moveSpeed;
         currentDirection = walkLeft;
+        scale = walkScale;
+        width = walkWidth;
+        height = walkHeight;
         hasMoved = true;
     }
-
-    if (swing) {
-        frameCount++;
-        if (frameCount >= frameLimit) {
-            frameCount = 0;
-            currentLoopIndex++;
-            if (currentLoopIndex >= swingCycle.length) {
-                currentLoopIndex = 0;
-            }
-    }}
 
     if (hasMoved) {
         frameCount++;
