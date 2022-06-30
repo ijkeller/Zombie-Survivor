0d019c320f6d92500ee1f975f604012e836cf573
Nicholas Leonard <nicholas_leonard@live.com>
2022-06-30 15:26:00 -0400

    added eslint and adjusted acordingly

--- player-movement.js
+++ player-movement.js
@@ -3,8 +3,8 @@
 let img = new Image();
 img.src = './assets/generated1.png';
 img.onload = function () {
-    window.requestAnimationFrame(gameLoop)
-}
+  window.requestAnimationFrame(gameLoop);
+};
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
@@ -46,15 +46,15 @@ let currentDirection = right;
 let currentAction = walkAction;
 
 function drawFrame(frameX, frameY, canvasX, canvasY) {
-    if (swing) {
-        ctx.drawImage(img,
-            frameX * width, frameY * height, width, height,
-            canvasX - 96, canvasY - 96, scale * width, scale * height);
-    } else {
-        ctx.drawImage(img,
-            frameX * width, frameY * height, width, height,
-            canvasX - 32, canvasY - 32, scale * width, scale * height);
-    }
+  if (swing) {
+    ctx.drawImage(img,
+      frameX * width, frameY * height, width, height,
+      canvasX - 96, canvasY - 96, scale * width, scale * height);
+  } else {
+    ctx.drawImage(img,
+      frameX * width, frameY * height, width, height,
+      canvasX - 32, canvasY - 32, scale * width, scale * height);
+  }
 }
 
 let keyPresses = {};
@@ -62,109 +62,109 @@ let counter = 0;
 
 window.addEventListener('keydown', keyDownListener, false);
 function keyDownListener(event) {
-    event.preventDefault;
-    keyPresses[event.key] = true;
-    counter = 0;
-    counter++;
-    if (counter > 4) {
-        return;
-    }
+  event.preventDefault;
+  keyPresses[event.key] = true;
+  counter = 0;
+  counter++;
+  if (counter > 4) {
+    return;
+  }
 }
 
 window.addEventListener('keyup', keyUpListener, true);
 function keyUpListener(event) {
-    event.preventDefault;
-    keyPresses[event.key] = false;
+  event.preventDefault;
+  keyPresses[event.key] = false;
 }
 
 function gameLoop() {
 
-    if (swing) {
-        ctx.clearRect(xPosition - 96, yPosition - 96, width, height);
-    } else {
-        ctx.clearRect(xPosition - 32, yPosition - 32, width, height);
+  if (swing) {
+    ctx.clearRect(xPosition - 96, yPosition - 96, width, height);
+  } else {
+    ctx.clearRect(xPosition - 32, yPosition - 32, width, height);
+  }
+
+  let hasMoved = false;
+
+  if (keyPresses.f) {
+    counter = 0;
+    counter++;
+    if (counter > 4) {
+      return;
     }
+    hasMoved = false;
+    keyPresses = {};
+    scale = swingScale;
+    width = swingWidth;
+    height = swingHeight;
+    currentCycle = swingCycle;
+    currentAction = swingAction;
+    currentLoopIndex = 0;
+    swing = true;
 
-    let hasMoved = false;
+  }
 
-    if (keyPresses.f) {
-        counter = 0;
-        counter++;
-        if (counter > 4) {
-            return;
-        }
-        hasMoved = false;
-        keyPresses = {};
-        scale = swingScale;
-        width = swingWidth;
-        height = swingHeight;
-        currentCycle = swingCycle;
-        currentAction = swingAction;
+  if (swing) {
+    frameCount++;
+    hasMoved = false;
+    if (frameCount >= frameLimit) {
+      frameCount = 0;
+      currentLoopIndex++;
+      if (currentLoopIndex >= swingCycle.length) {
         currentLoopIndex = 0;
-        swing = true;
-
-    }
-
-    if (swing) {
-        frameCount++;
-        hasMoved = false;
-        if (frameCount >= frameLimit) {
-            frameCount = 0;
-            currentLoopIndex++;
-            if (currentLoopIndex >= swingCycle.length) {
-                currentLoopIndex = 0;
-                swing = false;
-                width = walkWidth;
-                height = walkHeight;
-                currentAction = walkAction;
-            }
-        }
-    }
-
-    if (keyPresses.w) {
-        yPosition -= moveSpeed;
-        currentDirection = up;
-        scale = walkScale;
+        swing = false;
         width = walkWidth;
         height = walkHeight;
-        currentCycle = walkCycle;
-        hasMoved = true;
-    } else if (keyPresses.s) {
-        yPosition += moveSpeed;
-        currentDirection = down;
-        scale = walkScale;
-        width = walkWidth;
-        height = walkHeight;
-        hasMoved = true;
+        currentAction = walkAction;
+      }
     }
-    if (keyPresses.d) {
-        xPosition += moveSpeed;
-        currentDirection = right;
-        scale = walkScale;
-        width = walkWidth;
-        height = walkHeight;
-        hasMoved = true;
-    } else if (keyPresses.a) {
-        xPosition -= moveSpeed;
-        currentDirection = left;
-        scale = walkScale;
-        width = walkWidth;
-        height = walkHeight;
-        hasMoved = true;
+  }
+
+  if (keyPresses.w) {
+    yPosition -= moveSpeed;
+    currentDirection = up;
+    scale = walkScale;
+    width = walkWidth;
+    height = walkHeight;
+    currentCycle = walkCycle;
+    hasMoved = true;
+  } else if (keyPresses.s) {
+    yPosition += moveSpeed;
+    currentDirection = down;
+    scale = walkScale;
+    width = walkWidth;
+    height = walkHeight;
+    hasMoved = true;
+  }
+  if (keyPresses.d) {
+    xPosition += moveSpeed;
+    currentDirection = right;
+    scale = walkScale;
+    width = walkWidth;
+    height = walkHeight;
+    hasMoved = true;
+  } else if (keyPresses.a) {
+    xPosition -= moveSpeed;
+    currentDirection = left;
+    scale = walkScale;
+    width = walkWidth;
+    height = walkHeight;
+    hasMoved = true;
+  }
+
+  if (hasMoved) {
+    frameCount++;
+    if (frameCount >= frameLimit) {
+      frameCount = 0;
+      currentLoopIndex++;
+      if (currentLoopIndex >= walkCycle.length) {
+        currentLoopIndex = 0;
+      }
     }
+  }
 
-    if (hasMoved) {
-        frameCount++;
-        if (frameCount >= frameLimit) {
-            frameCount = 0;
-            currentLoopIndex++;
-            if (currentLoopIndex >= walkCycle.length) {
-                currentLoopIndex = 0;
-            }
-        }
-    }
+  drawFrame(currentCycle[currentLoopIndex], currentDirection + currentAction, xPosition, yPosition);
 
-    drawFrame(currentCycle[currentLoopIndex], currentDirection + currentAction, xPosition, yPosition);
-
-    window.requestAnimationFrame(gameLoop);
+  window.requestAnimationFrame(gameLoop);
 }
